286631cd10f6894335f099c168aa1fd3f9ace0dc
Bexdog <league@WTS-iMac-11.attlocal.net>
2019-02-12 19:46:11 -0800

    happy pet, pi aloud, change calculator

--- src/charAt_scanners_optionDialogs/HappyPet.java
+++ src/charAt_scanners_optionDialogs/HappyPet.java
@@ -1,31 +1,91 @@
 package charAt_scanners_optionDialogs;
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class HappyPet {
 	
-	// 2. Add the following variable to the next line: static int happinessLevel = 0;
+	 // 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	int happinessLevel = 0;
+	static int happinessLevel = 0;
 	public static void main(String[] args) {
+		Random BOb = new Random();
+	int bOb =	BOb.nextInt(10);
+		
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
 String bob = JOptionPane.showInputDialog("What pet do you want to buy?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
-			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
+	// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
+for (int i=-0;i<bOb;i++) {
 int Bob = JOptionPane.showOptionDialog(null, "How do you want to make your pet happy?", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-		new String[] { "cuddle", "food", "water", "walk", "groom", "clean up poop" }, null);
+		new String[] { "cuddle", "feed", "give water", "walk", "groom", "clean up poop" }, null);
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
 			
-
+System.out.println(Bob);
 			// 5. Use user input to call the appropriate method created in step 4.
+if (Bob == 1) {
+cleanUpPoop();	
+}
+if (Bob == 2) {
+cuddle();	
+}
+
+if (Bob == 3) {
+food();	
+}
+
+if (Bob == 4) {
+water();	
+}
+
+if (Bob == 5) {
+walk();	
+}
+
+if (Bob == 6) {
+groom();	
+}
 
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+if(happinessLevel==bOb) {
+	JOptionPane.showMessageDialog(null, "Your "+bob+" loves you");
+}
+}
 	}
 
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+
+static void cuddle() {
+happinessLevel=happinessLevel+1;	
 }
+static void cleanUpPoop() {
+happinessLevel=happinessLevel+1;	
+}
+
+static void groom() {
+happinessLevel=happinessLevel+1;	
+}
+
+static void walk() {
+happinessLevel=happinessLevel+1;	
+}
+
+static void water() {
+happinessLevel=happinessLevel+1;	
+}
+
+static void food() {
+happinessLevel=happinessLevel+1;	
+}
+
+
+}
+
+
+
+
--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -15,17 +15,53 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
-		// Convert their answer to an int using Integer.parseInt()
-
+String n = JOptionPane.showInputDialog("how many nickels do you have?");
+int N =Integer.parseInt(n);
+//Convert their answer to an int using Integer.parseInt()
 		// Ask the user how many dimes they have, and convert their answer
-
+String d = JOptionPane.showInputDialog("how many dimes do you have?");
+int D=Integer.parseInt(d);
 		// Ask the user how many quarters they have, and convert their answer
-
+String q = JOptionPane.showInputDialog("how many quarters do you have?");
+int Q=Integer.parseInt(q);
 		// Calculate how much money the user has and save it in a double variable 
-
+double money =(Q*25+D*10+N*5)*.01;
 		// Tell the user how much money they have
-
+JOptionPane.showMessageDialog(null, "you have $"+money);
+String sandwich = JOptionPane.showInputDialog("what type of sandwich do you want?");
+if(sandwich.equals("tuna")) {
+	JOptionPane.showMessageDialog(null, "I made you a tuna sandwich");
+}
+else if(sandwich.equals("peanut butter and jelly")) {
+	JOptionPane.showMessageDialog(null, "I made you a peanut butter and jelly sandwich");
+}
+else if(sandwich.equals("banana")) {
+	JOptionPane.showMessageDialog(null, "I made you a banana sandwich");
+}
+else if(sandwich.equals("hotdog")) {
+	JOptionPane.showMessageDialog(null, "I made you a hotdog");
+}
+else if(sandwich.equals("salami and bacon")) {
+	JOptionPane.showMessageDialog(null, "I made you a salami and bacon sandwich");
+}
+else if(sandwich.equals("french dip")) {
+	JOptionPane.showMessageDialog(null, "I made you a french dip sandwich");
+}
+else if(sandwich.equals("egg salad")) {
+	JOptionPane.showMessageDialog(null, "I made you an egg salad sandwich");
+}
+else if(sandwich.equals("ham and cheese")) {
+	JOptionPane.showMessageDialog(null, "I made you a ham and cheese sandwich");
+}
+else if(sandwich.equals("poop")) {
+	JOptionPane.showMessageDialog(null, "I made you a poop sandwich");
+}
+else if(sandwich.equals("human")) {
+	JOptionPane.showMessageDialog(null, "I made you a human sandwich (are you a cannibal?)");
+}
+else {
+	JOptionPane.showMessageDialog(null, "I can't make that kind of sandwich yet");
+}
 	}
 }
 
